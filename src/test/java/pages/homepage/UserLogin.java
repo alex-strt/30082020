11c9e519e836f7f2ac888f42f452e87dd311ae21
AlfaBravo <alexandrbedya@gmail.com>
2020-09-22 10:32:39 +0300

    added popup killer and stopper :(

--- src/test/java/pages/homepage/UserLogin.java
+++ src/test/java/pages/homepage/UserLogin.java
@@ -1,7 +1,6 @@
 package pages.homepage;
 
 import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selectors.byXpath;
